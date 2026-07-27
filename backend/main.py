02ae64891c2411e165305d1729cacb3325926e0c
Shashidhara M N <shashidharamnshshidharamn@gmail.com>
2026-07-27 12:45:39 +0530

    Added history API

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel
 from db import get_connection
 
 app = FastAPI()
+
 # -----------------------------
 # Enable CORS
 # -----------------------------
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 # -----------------------------
 # Data Model
 # -----------------------------
@@ -38,8 +39,8 @@ class SensorData(BaseModel):
     power_y: float
 
     status: str
-
     error_message: str
+
 
 # -----------------------------
 # Home API
@@ -59,43 +60,45 @@ def receive_data(data: SensorData):
     cursor = conn.cursor()
 
     cursor.execute("""
-               INSERT INTO sensor_data
-                    (
-                        vr,
-                        vy,
-                        vb,
-                        ir,
-                        iy,
-                        frequency,
-                        pf_r,
-                        pf_y,
-                        pf_b,
-                        pf_total,
-                        power_r,
-                        power_y,
-                        status,
-                        error_message
-                    )
+        INSERT INTO sensor_data
+        (
+            vr,
+            vy,
+            vb,
+            ir,
+            iy,
+            frequency,
+            pf_r,
+            pf_y,
+            pf_b,
+            pf_total,
+            power_r,
+            power_y,
+            status,
+            error_message,
+            created_at
+        )
         VALUES
-                (
-                    %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
-                )
+        (
+            %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
+            CURRENT_TIMESTAMP AT TIME ZONE 'Asia/Kolkata'
+        )
     """, (
-    data.vr,
-    data.vy,
-    data.vb,
-    data.ir,
-    data.iy,
-    data.frequency,
-    data.pf_r,
-    data.pf_y,
-    data.pf_b,
-    data.pf_total,
-    data.power_r,
-    data.power_y,
-    data.status,
-    data.error_message
-))
+        data.vr,
+        data.vy,
+        data.vb,
+        data.ir,
+        data.iy,
+        data.frequency,
+        data.pf_r,
+        data.pf_y,
+        data.pf_b,
+        data.pf_total,
+        data.power_r,
+        data.power_y,
+        data.status,
+        data.error_message
+    ))
 
     conn.commit()
 
@@ -118,26 +121,26 @@ def latest_data():
     cursor = conn.cursor()
 
     cursor.execute("""
-                       SELECT
-                            id,
-                            vr,
-                            vy,
-                            vb,
-                            ir,
-                            iy,
-                            frequency,
-                            pf_r,
-                            pf_y,
-                            pf_b,
-                            pf_total,
-                            power_r,
-                            power_y,
-                            status,
-                            error_message,
-                            created_at
-                        FROM sensor_data
-                        ORDER BY id DESC
-                        LIMIT 1;
+        SELECT
+            id,
+            vr,
+            vy,
+            vb,
+            ir,
+            iy,
+            frequency,
+            pf_r,
+            pf_y,
+            pf_b,
+            pf_total,
+            power_r,
+            power_y,
+            status,
+            error_message,
+            created_at
+        FROM sensor_data
+        ORDER BY id DESC
+        LIMIT 1;
     """)
 
     row = cursor.fetchone()
@@ -149,20 +152,76 @@ def latest_data():
         return {"message": "No Data Available"}
 
     return {
-    "id": row[0],
-    "vr": row[1],
-    "vy": row[2],
-    "vb": row[3],
-    "ir": row[4],
-    "iy": row[5],
-    "frequency": row[6],
-    "pf_r": row[7],
-    "pf_y": row[8],
-    "pf_b": row[9],
-    "pf_total": row[10],
-    "power_r": row[11],
-    "power_y": row[12],
-    "status": row[13],
-    "error_message": row[14],
-    "created_at": row[15]
-}
+        "id": row[0],
+        "vr": row[1],
+        "vy": row[2],
+        "vb": row[3],
+        "ir": row[4],
+        "iy": row[5],
+        "frequency": row[6],
+        "pf_r": row[7],
+        "pf_y": row[8],
+        "pf_b": row[9],
+        "pf_total": row[10],
+        "power_r": row[11],
+        "power_y": row[12],
+        "status": row[13],
+        "error_message": row[14],
+        "created_at": row[15]
+    }
+@app.get("/history")
+def history():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT
+            id,
+            vr,
+            vy,
+            vb,
+            ir,
+            iy,
+            frequency,
+            pf_r,
+            pf_y,
+            pf_b,
+            pf_total,
+            power_r,
+            power_y,
+            status,
+            error_message,
+            created_at
+        FROM sensor_data
+        ORDER BY id DESC;
+    """)
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    result = []
+
+    for row in rows:
+        result.append({
+            "id": row[0],
+            "vr": row[1],
+            "vy": row[2],
+            "vb": row[3],
+            "ir": row[4],
+            "iy": row[5],
+            "frequency": row[6],
+            "pf_r": row[7],
+            "pf_y": row[8],
+            "pf_b": row[9],
+            "pf_total": row[10],
+            "power_r": row[11],
+            "power_y": row[12],
+            "status": row[13],
+            "error_message": row[14],
+            "created_at": row[15]
+        })
+
+    return result
