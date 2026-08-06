08625c6d2c7573bb2f62157b15559e2a762d7f79
Shashidhara M N <shashidharamnshshidharamn@gmail.com>
2026-08-06 08:59:46 +0530

    Enable SSL

--- backend/db.py
+++ backend/db.py
@@ -1,14 +1,10 @@
+import os
 import psycopg2
 
-DATABASE_URL = "postgresql://mvpanel_user:t7co7eZr3yoBjqhd1aNyKVXtA8dCHbc8@dpg-d95or0po3t8c73cct070-a.virginia-postgres.render.com/mvpanel"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
-    return psycopg2.connect(DATABASE_URL)
-
-try:
-    conn = get_connection()
-    print("✅ Connected to PostgreSQL successfully!")
-    conn.close()
-except Exception as e:
-    print("❌ Connection failed")
-    print(e)
+    return psycopg2.connect(
+        DATABASE_URL,
+        sslmode="require"
+    )
